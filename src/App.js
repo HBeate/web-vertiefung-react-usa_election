fb1c29ce7fb36b62206abc141502eb9103c3150b
HBeate <hauser_b@hotmail.com>
2020-11-12 21:20:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { render } from '@testing-library/react';
+import { Component } from 'react';
+import './App.css';
+import Vote from './components/Vote';
+
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      trumpVotes: 0,
+      bidenVotes: 0
+    }
+  }
+
+  votes = (numberOfVotes, candidate) => {
+    if (candidate == "Trump") {
+      this.setState({
+        trumpVotes: numberOfVotes
+      })
+    } else {
+      this.setState({
+        bidenVotes: numberOfVotes
+      })
+    }
+  }
+
+  state = {}
+  render() {
+    return (<div className="App">
+
+      <div>    <h1> Total Votes: {this.state.bidenVotes + this.state.trumpVotes}</h1>  </div>
+      <div className="voteContainer">
+
+        <Vote name="Trump" onIncrement={this.votes} />
+        <Vote name="Biden" onIncrement={this.votes} />
+      </div>
+
+    </div>);
+  }
 }
+
+
 
 export default App;
